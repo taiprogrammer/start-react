8909d75e154b1a3abdd1740cc13811f30ce24851
taiprogrammer <taiza.marques@bandtec.com.br>
2021-03-25 22:43:19 -0300

    second project with react

---
+++ React-npx/music-box/src/App.js
@@ -0,0 +1,11 @@
+import NavBar from './components/NavBar'
+import './assets/css/style.css'
+import './assets/css/reset.css'
+
+function App() {
+  return (
+    <h2><NavBar/></h2>
+  );
+}
+
+export default App;
